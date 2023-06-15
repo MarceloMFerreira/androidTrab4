e8547c42b6340e7e43c2876ef0c7995ab1d2cf5b
MarceloMFerreira <marcelomf.007@hotmail.com>
2023-06-14 21:41:30 -0300

    first commit

---
+++ app/src/main/java/br/edu/ifsuldeminas/trab4/MainActivity.java
@@ -0,0 +1,38 @@
+package br.edu.ifsuldeminas.trab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText searchEditText;
+    private Button searchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchEditText = findViewById(R.id.searchEditText);
+        searchButton = findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchQuery = searchEditText.getText().toString().trim();
+                if (searchQuery.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Por favor, insira um termo de busca",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, WallpaperActivity.class);
+                    intent.putExtra("searchQuery", searchQuery);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/br/edu/ifsuldeminas/trab4/WallpaperActivity.java
@@ -0,0 +1,92 @@
+package br.edu.ifsuldeminas.trab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.IOException;
+
+public class WallpaperActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button wallpaperButton;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallpaper);
+
+        imageView = findViewById(R.id.imageView);
+        wallpaperButton = findViewById(R.id.wallpaperButton);
+        progressBar = findViewById(R.id.progressBar);
+
+        String searchQuery = getIntent().getStringExtra("searchQuery");
+        if (searchQuery != null) {
+            new DownloadWallpaperTask().execute(searchQuery);
+        }
+
+        wallpaperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(WallpaperActivity.this);
+                try {
+                    wallpaperManager.setBitmap(bitmap);
+                    Toast.makeText(WallpaperActivity.this, "Papel de parede definido com sucesso",
+                            Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(WallpaperActivity.this, "Erro ao definir o papel de parede",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private class DownloadWallpaperTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            String searchQuery = params[0];
+            try {
+                return Glide.with(WallpaperActivity.this)
+                        .asBitmap()
+                        .load(searchQuery)
+                        .apply(new RequestOptions()
+                                .diskCacheStrategy(DiskCacheStrategy.NONE))
+                        .submit()
+                        .get();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            progressBar.setVisibility(View.GONE);
+            if (bitmap != null) {
+                imageView.setImageBitmap(bitmap);
+            } else {
+                Toast.makeText(WallpaperActivity.this, "Falha ao carregar o papel de parede",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
